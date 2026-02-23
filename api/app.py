69625a2b9b5615d1fd7a8623b636399dccaa21a1
aakashascend-cell <aakashascend@gmail.com>
2026-02-23 14:25:04 +0530

    Creating an API that will monitor the user responses and predict churn status

--- api/app.py
+++ api/app.py
@@ -1,25 +1,32 @@
 from fastapi import FastAPI
-from schemas import CustomerData
+from .schemas import CustomerData
 import sys
 from pathlib import Path
 import joblib
+import pandas as pd
 
+model = None  # Global
 app = FastAPI(title="StreamFlix Churn Predictor")
 
 @app.on_event('startup')
 def load_model_on_startup():
-    prod_model = joblib.load(f'{(Path(__file__).parent.parent)}/models/best_model.pkl')
-    return prod_model
+    global model
+    model = joblib.load(f"{(Path(__file__).parent.parent)}/models/best_model.pkl")
+    
 
 @app.get("/")
 def health_check():
     return {"status": "healthy", "message": "API running"}
-    pass
+    
 
-
-
+@app.post("/predict")
+def predict_churn_endpoint(customer: CustomerData):
+    customer_dict = customer.model_dump()
+    customer_df = pd.DataFrame([customer_dict])
+    result = model.predict(customer_df)
+    return {"prediction": int(result[0])}
 
 if __name__ == '__main__':
-    print(f"Current WD {Path(__file__).parent.parent}")
-    pass
+    model = load_model_on_startup()
+    
     
